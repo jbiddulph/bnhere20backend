bbf3ebf2bf3e42408e22b54b01d1b83b4dcc3a19
John Biddulph <johnbiddulph@johns-mbp-2.home>
2019-12-19 21:43:05 +0000

    first commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        Model::unguard();
+        $this->call(ArticlesTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        Model::reguard();
+
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+
+Route::group([
+
+    'middleware' => 'api'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// List articles
+Route::get('articles', 'ArticleController@index');
+//List single article
+Route::get('article/{id}', 'ArticleController@show');
+//Create New Article
+Route::post('article', 'ArticleController@store');
+//Update Article
+Route::put('article', 'ArticleController@store');
+//Delete Article
+Route::delete('article/{id}', 'ArticleController@destroy');
+
